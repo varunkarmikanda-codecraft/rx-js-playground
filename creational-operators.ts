ef99c959593732b99e41393f1a21781b2233c687
Varun <varun.karmikanda@codecraft.co.in>
2026-05-04 13:48:51 +0530

    Creational op
    * Implement Observable creation functions: of, from, range, interval, and timer; update tests and package scripts
    
    * Implement double, square, and first operators; enhance Observable subscription management

--- creational-operators.ts
+++ creational-operators.ts
@@ -1,22 +1,71 @@
 import { Observable } from './observable.js';
 
 function of<T>(...values: T[]): Observable<T> {
-  //TODO:
+  // Create an observable
+  const observable = new Observable<T>((observer) => {
+    // Produce value from the received value
+    for(const value of values) {
+      // tell the observer that the value is available
+      observer.next(value);
+    }
+    // we are done producing values
+    observer.complete?.();
+    return null;
+  })
+  return observable;
 }
 
 function from<T>(iterable: Iterable<T>): Observable<T> {
-  // TODO:
+  const observable = new Observable<T>((observer) => {
+    for(const value of iterable) {
+      observer.next(value);
+    }
+    observer.complete?.();
+    return null;
+  })
+  return observable;
 }
 
 function range(start: number, count: number): Observable<number> {
-  // TODO:
+  const observable = new Observable((observer) => {
+    for(let s = start; s <= count; s++) {
+      observer.next(s);
+    }
+    observer.complete?.();
+    return null;
+  })
+
+  // implicitly conveying that there is no cleanup function needed
+
+  return observable;
 }
 
-function interval(period: number): Observable<number> {
-  // TODO:
+function interval(milliseconds: number): Observable<number> {
+  const observable = new Observable((observer) => {
+    let count = 0;
+    const interval = setInterval(() => {
+      // convey the current  value of the observer
+      observer.next(count++);
+    }, milliseconds);
+
+    // now forced to return a proper cleanup or a cleanup is not needed
+    return () => clearInterval(interval)
+  })
+
+  return observable;
 }
 
-function timer(dueTime: number): Observable<number> {
-  // TODO:
+function timer(dueTimeMilliseconds: number): Observable<number> {
+  const observable = new Observable((observer) => {
+    const timeout = setTimeout(() => {
+      // convey the current  value of the observer
+      observer.next();
+    }, dueTimeMilliseconds);
+
+    // now forced to return a proper cleanup or a cleanup is not needed
+    return () => clearTimeout(timeout)
+  })
+
+  return observable;
 }
 export { of, from, range, interval, timer };
--- pipeable-operators.ts
+++ pipeable-operators.ts
@@ -1,1 +1,83 @@
-// TODO:
+import { of } from "./creational-operators.js";
+import { Observable } from "./observable.js";
+
+// An operator is a function that takes an observable and return an observable
+type Operator<T, U> = (input$: Observable<T>) => Observable<U>;
+
+// lets implement an operator double
+const double: Operator<number, number> = (input$: Observable<number>): Observable<number> => {
+  
+  // Create the output observable. Its executor function must read the values of the input and double them,
+  // and feed the output to the observer of the output observable
+  // You get the reference to it in the executor function
+
+  const output$ = new Observable((observer) => {
+    const inputSubscription = input$.subscribe({
+      next(value) {
+        if(value !== undefined) {
+          observer.next(value * 2);
+        }
+      },
+    })
+
+    return () => {
+      inputSubscription.unsubscribe();
+    }
+  })
+
+  return output$;
+
+}
+
+const square: Operator<number, number> = (input$: Observable<number>): Observable<number> => {
+  const output$ = new Observable((observer) => {
+    const inputSubscription = input$.subscribe({
+      next(value) {
+        if(value !== undefined) {
+          observer.next(value * value);
+        }
+      },
+    })
+
+    return () => {
+      inputSubscription.unsubscribe();
+    }
+  })
+
+  return output$;
+}
+
+const first: Operator<unknown, unknown> = (input$: Observable<unknown>): Observable<unknown> => {
+  const output$ = new Observable<unknown>((observer) => {
+    const inputSubscription = input$.subscribe({
+      next(value: unknown) {
+        observer.next(value);
+        // Soon after the consumption of the first value we need to unsubscribe
+        inputSubscription.unsubscribe()
+      },
+    })
+    return () => inputSubscription.unsubscribe();
+  })
+  return output$;
+}
+
+
+// we can actually chain these - compose or pipe
+
+// We can create an observable that doubles and then squares a given observable and gets the first value.
+
+const anObservable$ = of(1, 2, 3, 4);
+
+const firstDoubledSquare$ = first(square(double(anObservable$)))
+firstDoubledSquare$.subscribe({
+  next(value) {
+    console.log("From manual piping: ", value);
+  }
+})
+
+const firstDoubledSquare1$ = anObservable$.pipe(double, square, first);
+firstDoubledSquare1$.subscribe({
+  next(value) {
+    console.log("From observables piping: ", value);
+  },
+})
